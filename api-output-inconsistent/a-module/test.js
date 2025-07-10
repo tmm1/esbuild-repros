021a61c011932baa064e5c89bcb170f28ac4855b
Aman Karmani <aman@tmm1.net>
2025-07-10 10:28:14 -0700

    repro try

---
+++ api-output-inconsistent/a-module/test.js
@@ -0,0 +1,24 @@
+import esbuild from 'esbuild'
+
+async function main() {
+	const input = `
+	import type { Accessor as SolidAccessor } from './solid/solid.js';
+	import { createStore as solidCreateStore } from './solid/store.js';
+	export { SolidAccessor, solidCreateStore };
+	`
+	const options = {
+		target: ['es2022'],
+		format: 'esm',
+		platform: undefined,
+		loader: 'ts',
+		supported: {
+			'class-static-blocks': false,
+			'dynamic-import': true,
+			'class-field': true
+		}
+	}
+	const result = await esbuild.transform(input, options)
+	console.log(result.code)
+}
+
+main()
