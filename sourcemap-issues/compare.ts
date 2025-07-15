8e7b79f4a30c7bb8dbecb58d3383a2ef4cab76ce
Aman Karmani <aman@tmm1.net>
2025-07-15 13:23:11 -0700

    another example

--- sourcemap-issues/compare.ts
+++ sourcemap-issues/compare.ts
@@ -10,7 +10,21 @@ function lcFrom(position: sm.Position): ts.LineAndCharacter {
     return { line: position.line - 1, character: position.column };
 }
 
-const c = {
+function calc(mapfile: string, srcfile: string, c: any) {
+    const sourcemap = fs.readFileSync(mapfile, 'utf8');
+    const smc = new sm.SourceMapConsumer(sourcemap);
+    //smc.eachMapping((m) => { console.log(m) })
+    const positionFrom = mappedPositionFrom.bind(null, srcfile);
+
+    const start = lcFrom(smc.generatedPositionFor(positionFrom(c.range.start)));
+    const end = lcFrom(smc.generatedPositionFor(positionFrom(c.range.end)));
+    console.log()
+    console.log(mapfile)
+    console.log(start, end)
+    console.log(start.character != end.character)
+}
+
+const ptyHost = {
     range: {
         start: {
             line: 12,
@@ -22,21 +36,20 @@ const c = {
         }
     }
 }
+calc('out-esbuild/ptyHostMain.js.map', '../src/ptyHostMain.ts', ptyHost)
+calc('out-tsc/ptyHostMain.js.map', '../src/ptyHostMain.ts', ptyHost)
 
-
-function calc(mapfile: string) {
-    const sourcemap = fs.readFileSync(mapfile, 'utf8');
-    const smc = new sm.SourceMapConsumer(sourcemap);
-    //smc.eachMapping((m) => { console.log(m) })
-    const positionFrom = mappedPositionFrom.bind(null, '../src/ptyHostMain.ts');
-
-    const start = lcFrom(smc.generatedPositionFor(positionFrom(c.range.start)));
-    const end = lcFrom(smc.generatedPositionFor(positionFrom(c.range.end)));
-    console.log()
-    console.log(mapfile)
-    console.log(start, end)
-    console.log(start.character != end.character)
+const test = {
+    range: {
+        start: {
+            line: 2,
+            character: 14
+        },
+        end: {
+            line: 2,
+            character: 31
+        }
+    }
 }
-
-calc('out-esbuild/ptyHostMain.js.map')
-calc('out-tsc/ptyHostMain.js.map')
+calc('out-esbuild/test.js.map', '../src/test.ts', test)
+calc('out-tsc/test.js.map', '../src/test.ts', test)
